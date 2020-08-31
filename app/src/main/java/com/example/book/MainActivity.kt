1e4ac886935cfa74ff7b9d6914d58d2857ae7bcf
Vasilevskiy Pasha <vasilevskij00@inbox.ru>
2020-08-31 15:20:47 +0300

    init: add simple menu

---
+++ app/src/main/java/com/example/book/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.book
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
